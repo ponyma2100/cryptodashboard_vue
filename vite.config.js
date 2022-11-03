69537e3c13b431b6e08a0a4cdd6c70c06346db25
PonyMa <ponyma2100@gmail.com>
2022-11-03 17:43:59 +0800

    fixed: Modify config

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base:
+    process.env.NODE_ENV === "production" ? "/<cryptodashboard_vue>/" : "./",
 });
