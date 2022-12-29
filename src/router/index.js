47d46a9440056ab50d3b06d4412f951c9b3f46a8
PonyMa <ponyma2100@gmail.com>
2022-12-30 00:38:12 +0800

    Add ga

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
-    { path: '/', component: Home, redirect:{name:'home'}},
+    { path: "/", component: Home, redirect: { name: "home" } },
     {
       path: "/home",
       name: "home",
